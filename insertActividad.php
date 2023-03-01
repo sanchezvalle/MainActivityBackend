b500ae49d3b604aff8691df58523152b5add6f19
fjsanchezv10 <98148806+fjsanchezv10@users.noreply.github.com>
2023-03-01 09:50:55 +0100

    Updated: insertActividad.php

--- insertActividad.php
+++ insertActividad.php
@@ -9,12 +9,12 @@ $descripcion_post = $_POST['descripcion'];
 $tipo_post = $_POST['tipo'];
 $rangoedad = $_POST['rangoedad'];
 
-$sql = "INSERT INTO evento('id_poblacion','nombre','descripcion','id_tipo','calificacion') 
+$sql = "INSERT INTO evento(`id_poblacion`,`nombre`,`descripcion`,`id_tipo`,`calificacion`) 
             VALUES ((SELECT id FROM poblacion WHERE nombre LIKE '$nombrePoblacion_post'), 
             '$nombreEvento_post', '$descripcion_post', (SELECT id FROM tipo_evento WHERE nombre LIKE '$tipo_post'), 0.0)";
 $connection->query($sql) or die("Fallo en la ejecución de la sentencia");
 
-$sqlre = "INSERT INTO evento_rangoedad('id_evento','id_rangoedad') 
+$sqlre = "INSERT INTO evento_rangoedad(`id_evento`,`id_rangoedad`) 
             VALUES ((SELECT id FROM evento WHERE nombre LIKE '$nombreEvento_post'), 
                     (SELECT id FROM rango_edad WHERE rango LIKE '$rangoedad'))";
 $connection->query($sqlre) or die("Fallo en la ejecución de la inserción del rango edad");
